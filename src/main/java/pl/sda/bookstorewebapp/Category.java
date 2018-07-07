9d87cf2eeef938b62ceb8b963a61202eceaae927
Jakub  Dobrogowski <dobrogowskijakub@gmail.com>
2018-07-07 23:27:55 +0200

    Category

---
+++ src/main/java/pl/sda/bookstorewebapp/Category.java
@@ -0,0 +1,24 @@
+package pl.sda.bookstorewebapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// dzieki temu nie musimy pisac kodow
+// getterow i setterow w klasie, robi to
+// kompilator w trakcie kompilacji, dzieki
+// bibliotece Lombok
+@Setter
+@Getter
+@AllArgsConstructor
+public class Category {
+    private Integer id;
+    private String name;
+    private Integer parentId;
+
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
